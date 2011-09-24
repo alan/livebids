a7faae4aff4708d6d7478cc589841c8baf1d680c
Jason Lee <jlsync@gmail.com>
2011-09-24 18:34:11 +0100

    add 127.0.0.1  local.host to /etc/hosts then use http://local.host:8080/ for development

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 var settings = {
 	'sessionSecret': 'sessionSecret'
 	, 'port': 8080
-	, 'uri': 'http://localhost:8080' // Without trailing /
+	, 'uri': 'http://local.host:8080' // Without trailing /
 
 	// You can add multiple recipiants for notifo notifications
 	, 'notifoAuth': null /*[
@@ -13,8 +13,9 @@ var settings = {
 
 	, 'external': {
 		 'twitter': {
-			consumerKey: 'PyvdOrZcZsn4qRUy9jbElQ',
-			consumerSecret: 'ahHfn56kKVchJnwcA157XGJeIXVRMiWENSMx5WGJdg'
+      // local.host:8080
+			consumerKey: '9EVsAL0C8ktXRUebb5I5hw',
+			consumerSecret: '8ZgpyOZAJuBDxqcLLS56Y6D94NS3Q4gk7A3H4Ttlo'
 		}
 	}
 
@@ -25,6 +26,10 @@ var settings = {
 if (process.env.NODE_ENV == 'production') {
 	settings.uri = 'http://livebids.herokuapp.com';
 	settings.port = process.env.PORT || 80; // Joyent SmartMachine uses process.env.PORT
+
+  // livebids.heroku.com
+  settings.external.twitter.consumerKey = 'PyvdOrZcZsn4qRUy9jbElQ';
+  settings.external.twitter.consumerSecret =  'ahHfn56kKVchJnwcA157XGJeIXVRMiWENSMx5WGJdg';
 
 	//settings.airbrakeApiKey = '0190e64f92da110c69673b244c862709'; // Error logging, Get free API key from https://airbrakeapp.com/account/new/Free
 }
