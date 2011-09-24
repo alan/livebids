0667e13192e6f4e481488696e9ce37930f97b139
Jason Lee <jlsync@gmail.com>
2011-09-24 16:29:22 +0100

    twitter

--- siteConfig.js
+++ siteConfig.js
@@ -11,24 +11,15 @@ var settings = {
 		}
 	]*/
 
-	/*
-	// Enter API keys to enable auth services, remove entire object if they aren't used.
 	, 'external': {
-		'facebook': {
-			appId: '123983866527489',
-			appSecret: '6edf1327ege27bbba2e239f73cd866c4'
-		}
-		, 'twitter': {
-			consumerKey: 'eA54JQ6rtdZE7nqaRa6Oa',
-			consumerSecret: '6u2makgFdf4F6EauP7osa54L34SouU6eLgaadTD435Rw'
-		}
-		, 'github': {
-			appId: '1444g6a7d26a3f716b47',
-			appSecret: 'e84f13367f328da4b8c96a4f74gfe7e421b6a206'
+		 'twitter': {
+			consumerKey: 'PyvdOrZcZsn4qRUy9jbElQ',
+			consumerSecret: 'ahHfn56kKVchJnwcA157XGJeIXVRMiWENSMx5WGJdg'
 		}
 	}
-	*/
+
 	, 'debug': (process.env.NODE_ENV !== 'production')
+
 };
 
 if (process.env.NODE_ENV == 'production') {
